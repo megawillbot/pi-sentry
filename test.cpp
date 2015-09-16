78db5408856e355ffd8a72ae9b00f4a56bb861b4
megawillbot <megawillbot@gmail.com>
2015-09-16 05:29:55 +0000

    test

--- test.cpp
+++ test.cpp
@@ -1,9 +1,11 @@
+
 #include<opencv2/opencv.hpp>
 #include<iostream>
 #include<vector>
  
 int main(int argc, char *argv[])
 {
+    std::cout << "Hello";
     cv::Mat frame;
     cv::Mat back;
     cv::Mat fore;
@@ -19,13 +21,20 @@ int main(int argc, char *argv[])
  
     for(;;)
     {
+    std::cout << "inside for";
         cap >> frame;
         bg.operator ()(frame,fore);
         bg.getBackgroundImage(back);
+    std::cout << "got background image";
         cv::erode(fore,fore,cv::Mat());
         cv::dilate(fore,fore,cv::Mat());
+    std::cout << "done erode and dilate";
         cv::findContours(fore,contours,CV_RETR_EXTERNAL,CV_CHAIN_APPROX_NONE);
+    std::cout << ", found contours";
         cv::drawContours(frame,contours,-1,cv::Scalar(0,0,255),2);
+    std::cout << ", drawn contours";
+//        frame.save("frame.jpg");
+
         cv::imshow("Frame",frame);
         cv::imshow("Background",back);
         if(cv::waitKey(30) >= 0) break;
