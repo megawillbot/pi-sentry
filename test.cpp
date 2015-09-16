256f97e88c68a23121cf8298f9079f69ca779524
megawillbot <megawillbot@gmail.com>
2015-09-16 06:08:40 +0000

    Working on roughly quarter sized image, with maybe 1 second delay 	modified:   test.cpp

--- test.cpp
+++ test.cpp
@@ -7,36 +7,33 @@ int main(int argc, char *argv[])
 {
     std::cout << "Hello";
     cv::Mat frame;
+    cv::Mat frameSmaller;
     cv::Mat back;
     cv::Mat fore;
     cv::VideoCapture cap(0);
-    cv::BackgroundSubtractorMOG2 bg;
+    cv::BackgroundSubtractorMOG2 bg(10, false);
     //bg.nmixtures = 3;
     //bg.bShadowDetection = false;
  
     std::vector<std::vector<cv::Point> > contours;
  
-    cv::namedWindow("Frame");
-    cv::namedWindow("Background");
+    cv::namedWindow("FrameSmaller");
+    cv::namedWindow("Fore");
  
     for(;;)
     {
     std::cout << "inside for";
         cap >> frame;
-        bg.operator ()(frame,fore);
+        cv:resize(frame, frameSmaller, cv::Size(), 0.4,0.4, cv::INTER_NEAREST);
+        bg.operator ()(frameSmaller,fore);
         bg.getBackgroundImage(back);
-    std::cout << "got background image";
         cv::erode(fore,fore,cv::Mat());
         cv::dilate(fore,fore,cv::Mat());
-    std::cout << "done erode and dilate";
         cv::findContours(fore,contours,CV_RETR_EXTERNAL,CV_CHAIN_APPROX_NONE);
-    std::cout << ", found contours";
-        cv::drawContours(frame,contours,-1,cv::Scalar(0,0,255),2);
-    std::cout << ", drawn contours";
-//        frame.save("frame.jpg");
-
-        cv::imshow("Frame",frame);
-        cv::imshow("Background",back);
+        cv::drawContours(frameSmaller,contours,-1,cv::Scalar(0,0,255),2);
+        cv::imshow("FrameSmaller",frameSmaller);
+        cv::imshow("Fore",fore);
+//        cv::imshow("Background",back);
         if(cv::waitKey(30) >= 0) break;
     }
     return 0;
